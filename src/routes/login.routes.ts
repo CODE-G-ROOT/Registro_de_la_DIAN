235d5cae26084e277507f41fccdd7953874533d4
CODE-G-ROOT <juanavila8856@gmail.com>
2024-12-01 19:54:19 -0500

    feat: :sparkles: Solucion rutas de registro
    en el commit anterior se me olvidó agregar el archivo de las rutas
    
    b0700249d3b2536d903a96b8aa1901694051bed2

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { get_users, login_user} from "../controllers/login.controller";
+import { get_users, login_user, post_new_user} from "../controllers/login.controller";
 
 const login_router = Router();
 
 login_router.get("/get_users", get_users) // solo puede realizar la búsqeuda cuando tenga una cuenta y un rol
 
 login_router.post("/login", login_user)
-login_router.post("/register")
+login_router.post("/register", post_new_user)
 
 export default login_router;
